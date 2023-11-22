ecf55d12a14ff36415ce85611081fbecd2d1f042
Brian Jensen <brian.jensen@earthsoft.com>
2023-11-22 11:03:25 -0600

    cleans: App.css and App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,4 @@
 import { useState } from 'react';
-// import Project from './components/projects/projects';
-// import UpdateProject from './components/projects/updateProject';
-// import useProjects from './hooks/useProjects';
 import './App.css';
 import AppCalendar, { CalendarValue } from './components/calendar/calendar';
 
@@ -12,9 +9,6 @@ function App() {
   return (
     <div className='app-container'>
       <AppCalendar date={date} setDate={setDate} />
-      {/* <Calendar onChange={setDate} value={date} /> */}
-      {/* <Project projects={projects} createProject={createProject}></Project>
-      {projects[0] && <UpdateProject project={projects[0]} updateProject={updateProject}></UpdateProject>} */}
     </div>
   );
 }
